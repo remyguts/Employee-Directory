de728051b9f473cee21492759384931c439c6c0e
Remy Guts <remyguts@gmail.com>
2020-04-10 23:27:48 -0700

    added API URL to the code and eliminated the API key

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 // yo remy........ change this API tomorrow
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+const BASEURL = "https://randomuser.me/api/";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: (query) => axios.get(BASEURL + query + APIKEY),
+  search: (query) => axios.get(BASEURL + query),
 };
