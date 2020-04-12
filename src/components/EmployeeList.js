ca641529fa14d34d933ca608962714ae8663e462
Remy Guts <remyguts@gmail.com>
2020-04-12 12:30:01 -0700

    got rid od employee context. mpped and key reffed

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,23 @@
-import { createContext } from "react";
+import React from "react";
 
-export const EmployeeContext = createContext(null);
+function Employees(props) {
+  return (
+    <tbody>
+      {props.employees.map((employee) => (
+        <tr>
+          <td>
+            <img alt={`img`} src={employee.picture.medium}></img>
+          </td>
+          <td>
+            {employee.name.first} {employee.name.last}
+          </td>
+          <td>{employee.phone}</td>
+          <td>{employee.email}</td>
+          <td>{new Date(employee.dob.date).toLocaleDateString()}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+export default Employees;
