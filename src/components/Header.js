fddcae281f80212c9c0aa8e5b95b62372ecb8b50
Remy Guts <remyguts@gmail.com>
2020-04-12 12:23:00 -0700

    built up header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Filter by employees name or start date</p>
+    </div>
+  );
+}
+
+export default Header;
