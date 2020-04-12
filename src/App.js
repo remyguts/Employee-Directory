edce77e410323a6d5663388c5271278c7206e6ed
Remy Guts <remyguts@gmail.com>
2020-04-11 19:02:46 -0700

    started to rework appjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
-import Form from "./components/Form";
-import Employees from "./components/employee.json";
+  
+import React, {useState} from 'react';
+
+import './App.css';
+import Navbar from "./components/Navbar"
+import Table from "./components/Table"
+
+import { EmployeeContext } from "./components/EmployeeContext"
+
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [displayedEmployees, setDisplayedEmployees] = useState([])
 
+  return (
+    <div className="App">
+      <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}>
+        <Navbar />
+        <Table />
+      </EmployeeContext.Provider>
+    </div>
+  );
+}
 
-
-
-    return (
-
+export default App;
 
 
         
