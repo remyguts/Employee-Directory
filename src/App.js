d57ce76b8e740e50845d2a31b3fc5d3526fb2f95
Remy Guts <remyguts@gmail.com>
2020-04-12 11:56:20 -0700

    fixed my indexjs file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-  
-import React, {useState} from 'react';
+import React, { useState } from "react";
+//import "./App.css";
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
 
-import './App.css';
-import Navbar from "./components/Navbar"
-import Table from "./components/Table"
-
-import { EmployeeContext } from "./components/EmployeeContext"
+import { EmployeeContext } from "./components/EmployeeContext";
 
 function App() {
   const [employees, setEmployees] = useState([]);
-  const [displayedEmployees, setDisplayedEmployees] = useState([])
+  const [displayedEmployees, setDisplayedEmployees] = useState([]);
 
   return (
     <div className="App">
-      <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}>
+      <EmployeeContext.Provider
+        value={{
+          employees,
+          setEmployees,
+          displayedEmployees,
+          setDisplayedEmployees,
+        }}
+      >
         <Navbar />
         <Table />
       </EmployeeContext.Provider>
     </div>
   );
 }
-
-export default App;
-
-
-        
-    )
-        
-        
-        
-        
-        
-};
 
 export default App;
