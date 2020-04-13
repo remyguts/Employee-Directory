81e777a0076da37b082bc67393f9f555aa28242a
Remy Guts <remyguts@gmail.com>
2020-04-12 19:18:44 -0700

    Updates

---
+++ precache-manifest.c74c1f297f982ed5739472b48fe469eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00e815f2036a5d2be97043e643d9fca",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "0a8b084f2ca665d47bee",
+    "url": "/Employee-Directory/static/css/main.d7adb063.chunk.css"
+  },
+  {
+    "revision": "8f0faf50910c727b6670",
+    "url": "/Employee-Directory/static/js/2.8b486658.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.8b486658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8b084f2ca665d47bee",
+    "url": "/Employee-Directory/static/js/main.9bf1b43d.chunk.js"
+  },
+  {
+    "revision": "961815129ed41eb07899",
+    "url": "/Employee-Directory/static/js/runtime-main.9bb5a6c6.js"
+  }
+]);
